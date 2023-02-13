68acd3c9facace0bbf0655d85cae03649e9255c1
Studentabel <abelmorad97@gmail.com>
2023-02-13 09:26:59 +0800

    Updated workexperience content

--- script.js
+++ script.js
@@ -31,27 +31,34 @@ let prevScrollPos = window.pageYOffset;
 // loads the the first job
 window.onload = showEntrep()
 
-function showEntrep() {
+function showDev() {
     
-    document.getElementById("job-title").innerText = "Self-employed(Pizza Maker)"
-    document.getElementById("year").innerText = "December 2020- MAY 2022"
+    document.getElementById("job-title").innerText = "Front-end Web Developer"
+    document.getElementById("year").innerText = "2022 - Present"
     document.getElementById("job-description").innerHTML =
      `
     <li>
-        Sourced initial funding to get business up and running while planning initial
-        structure and making strategic first hires.
+        Volunteered to create a website for Lupon Kagan Islamic Welfare Association Inc.
     </li>
     <li>
-        Evaluated suppliers to maintain cost controls and improve operations.
+        Creates website using HTML, CSS, and JavaScript
+    </li>`
+}
+
+
+function showEntrep() {
+    
+    document.getElementById("job-title").innerText = "Self-employed(Pizza Maker)"
+    document.getElementById("year").innerText = "2020 - 2022"
+    document.getElementById("job-description").innerHTML =
+     `
+    <li>
+        Baked Neapolitan Pizza
     </li>
     <li>
         Created and updated company digital presence to enhance product
         marketing through professional website and optimized social media
         profiles.
-    </li>
-    <li>
-        Promoted positive customer experience through day-to-day supervision
-        and management of Abel's Pizza Co.
     </li>
     <li>
         Managed purchasing, sales, marketing and operations efficiently.
@@ -61,54 +68,29 @@ function showEntrep() {
 function showFarmer() {
     
     document.getElementById("job-title").innerText = "Banana Farmer"
-    document.getElementById("year").innerText = "October 2019 - May 2020"
+    document.getElementById("year").innerText = "2019 - 2020"
     document.getElementById("job-description").innerHTML = 
     `
     <li>
-        Applies the optimal amount of fertilizers every week.
+        Applies the optimal amount of fertilizers every week
     </li>
     <li>
-        Inspects for banana diseases and rehabilitate sick bananas.
-    </li>
-    <li>
-        Maintains the population of the suckers.
+        Inspects for banana diseases and rehabilitate sick bananas
     </li>`
 }
 
 function showTech() {
     
     document.getElementById("job-title").innerText = "IT Technician"
-    document.getElementById("year").innerText = "April 2017 - December 2018"
+    document.getElementById("year").innerText = "2017 - 2018"
     document.getElementById("job-description").innerHTML = 
     `
     <li>
-        Maintained officed PCs, network and mobile devices.
-    </li>
-    <li>
-        Refurbished PC systems and peripherals such as monitors, networking
-        equipment, printers, and more.
-    </li>
-    <li>
-        Configured systems according to prescribed software and hardware
-        frameworks.
-    </li>
-    <li>
-        Patiently walked individuals through basic troubleshooting tasks.
+        Patiently walked individuals through basic troubleshooting tasks
     </li>
     <li>
         Managed system-wide operating system and software deployments as
-        well as related software upgrade problems.
-    </li>
-    <li>
-        Monitored systems in operation and quickly troubleshot errors.
-    </li>
-    <li>
-        Tested and installed motherboards, processors and graphics cards on
-        desktops and laptops for corporate staff.
-    </li>
-    <li>
-        Explained technical information in clear terms to promote better
-        understanding for non-technical users.
+        well as related software upgrade problems
     </li>`
 }
 
