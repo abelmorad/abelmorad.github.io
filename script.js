2b00f0830b5e16e104e29da911dcffaff6bfe41d
Studentabel <abelmorad97@gmail.com>
2023-02-14 20:45:04 +0800

    removed loader script

--- script.js
+++ script.js
@@ -120,10 +120,3 @@ function hamBurger() {
     }
 
 }
-
-let loader = document.getElementById('pre-loader');
-
-window.addEventListener('load', function() {
-    loader.style.display = 'none';
-})
-
